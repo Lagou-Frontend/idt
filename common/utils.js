eec785619725d78d0d54711c23b67984143fd50b
Pooky <guananddu@qq.com>
2014-11-03 15:37:35 +0800

    针对ajax请求的判定

--- common/utils.js
+++ common/utils.js
@@ -12,6 +12,11 @@ module.exports = {
 
     isAjax: function ( req ) {
 
+        if ( req.headers[ 'x-requested-with' ]
+            && ( req.headers[ 'x-requested-with' ] == 'XMLHttpRequest' )
+            && ( req.url.substring( req.url.length - 3 ) != '.js' ) )
+            return 1;
+
         return req.url.substring( req.url.length - 4 ) == 'json';
 
     }
---
+++ mock/ajax/mycenter_showQRCode.js
@@ -0,0 +1,10 @@
+//mock your ajax data
+
+module.exports = {
+    "content": "",
+    "code": 0,
+    "success": false,
+    "resubmitToken": null,
+    "msg": null,
+    "requestId": null
+};
